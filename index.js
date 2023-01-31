aa1dd256af565807a16327d4d910f80f09bae493
berger <robertberger.dev@gmail.com>
2023-01-31 18:08:43 -0500

    saved and ran npm start

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const randomstring_1 = __importDefault(require("randomstring"));
-function generatePassword() {
-    let newPass = randomstring_1.default.generate(15);
-    return newPass;
-}
-;
-exports.default = generatePassword;
-console.log(generatePassword());
--- index.ts
+++
@@ -1,8 +0,0 @@
-import randomstring from 'randomstring';
-
-function generatePassword() {
-    let newPass = randomstring.generate(15);
-    return newPass;
-};
-
-export default generatePassword
