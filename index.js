13c17907daf92c879981cfddba318e840b5aabcd
berger <robertberger.dev@gmail.com>
2023-01-31 18:15:27 -0500

    downloaded moment and tested

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const moment_1 = __importDefault(require("moment"));
+console.log((0, moment_1.default)().format('LLLL'));
