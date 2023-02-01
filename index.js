5252a96b75a178c09d4b0c4b6f4edeab26939a29
berger <robertberger.dev@gmail.com>
2023-02-01 16:17:29 -0500

    created and tested new class User

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const passwordGenerator_1 = __importDefault(require("./passwordGenerator"));
-const dateMaker_1 = __importDefault(require("./dateMaker"));
-console.log((0, dateMaker_1.default)());
-console.log((0, passwordGenerator_1.default)());
+class User {
+    constructor(objUserId, objUsername, objCreatedDate) {
+        this.password = (0, passwordGenerator_1.default)();
+        this.username = objUsername;
+        this.userId = objUserId;
+        this.createdDate = objCreatedDate;
+    }
+    prettyPrint() {
+        return `---\n\nID: ${this.userId}\nUsername: ${this.username}\nPassword: ${this.password}\nCreated On: ${this.createdDate}`;
+    }
+}
+let mw = new User(1111, "Michael Weston", "today");
+console.log(mw.prettyPrint());
--- index.ts
+++ index.ts
@@ -1,5 +1,23 @@
 import generatePassword from './passwordGenerator'
 import generateDate from './dateMaker'
 
-console.log(generateDate());
-console.log(generatePassword());
+class User {
+    userId: number;
+    username: string;
+    password: string = generatePassword();
+    createdDate: string;
+
+    constructor(objUserId: number, objUsername: string, objCreatedDate: string) {
+        this.username = objUsername;
+        this.userId = objUserId;
+        this.createdDate = objCreatedDate;
+    }
+
+    prettyPrint(): string {
+        return `---\n\nID: ${this.userId}\nUsername: ${this.username}\nPassword: ${this.password}\nCreated On: ${this.createdDate}`
+    }
+}
+
+let mw: User = new User(1111, "Michael Weston", "today");
+
+console.log(mw.prettyPrint());
