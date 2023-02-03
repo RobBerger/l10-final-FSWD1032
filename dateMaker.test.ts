49d34a0363dbcd3c6d18c37e0519b58362cfb2d7
berger <robertberger.dev@gmail.com>
2023-02-03 10:40:28 -0500

    setup test for generateDate and tested the test

--- dateMaker.test.ts
+++ dateMaker.test.ts
@@ -1,2 +1,10 @@
-import { generateDate } from "./dateMaker";
+import generateDate from "./dateMaker";
+import moment from 'moment';
 
+describe('dateMaker', () => {
+
+    it('generateDate is makeing dates', () => {
+        let result = generateDate();
+        expect(result).toEqual(moment().format('LLLL'));
+    })
+})
